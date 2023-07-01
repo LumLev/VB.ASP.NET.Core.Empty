b6ae0deddd13842ebc42765565cca8f41b459a44
LumLev <fatlum.latif@outlook.com>
2023-07-02 01:16:04 +0200

    Static v

--- Program.vb
+++ Program.vb
@@ -4,7 +4,10 @@ Module Program
     Sub Main(args As String())
         Dim builder = WebApplication.CreateBuilder(args)
         Dim app = builder.Build()
-        app.MapGet("/", Function() "Welcome to VB.ASP.NET Core")
+        ' app.MapGet("/", Function() "Welcome to VB.ASP.NET Core")
+        app.UseDefaultFiles()
+        app.UseHttpsRedirection()
+        app.UseStaticFiles()
         app.Run()
     End Sub
 End Module
