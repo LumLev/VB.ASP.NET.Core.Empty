497d5ab4c28641bd06310b872654d968750c766f
LumLev <fatlum.latif@outlook.com>
2023-07-02 15:02:18 +0200

    added UseHsts for nondev envs

--- Program.vb
+++ Program.vb
@@ -4,7 +4,10 @@ Module Program
     Sub Main(args As String())
         Dim builder = WebApplication.CreateBuilder(args)
         Dim app = builder.Build()
-        ' app.MapGet("/", Function() "Welcome to VB.ASP.NET Core")
+
+        If app.Environment.IsDevelopment() Then
+            app.UseHsts()
+        End If
         app.UseDefaultFiles()
         app.UseHttpsRedirection()
         app.UseStaticFiles()
